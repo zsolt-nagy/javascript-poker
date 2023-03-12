d56da6ff9c89a4630787181c9d781e42f4ce5a3e
Zsolt Nagy <info@zsoltnagy.eu>
2023-03-12 19:34:40 +0100

    játékos lapjainak a húzása

---
+++ README.md
@@ -0,0 +1,13 @@
+# Poker JavaScript játék API segítségével
+
+## Felhasznált eszközök:
+
+- https://deckofcardsapi.com/ 
+- `button-15` a https://getcssscan.com/css-buttons-examples oldalról 
+
+## Korlátok
+
+Gyakorlati szempontból a DOM elemek felépítésekor `innerHTML` utasítást használtunk
+annak érdekében, hogy a kód egyszerűbb, érthetőbb legyen. Az `innerHTML` biztonsági
+korlátaival itt nem foglalkozunk, mert megbízunk a https://deckofcardsapi.com/ 
+tartalmában.
--- poker.js
+++ poker.js
@@ -1,1 +1,38 @@
-alert('Hello, JavaScript!');
+const newGameButton = document.querySelector('.js-new-game-button');
+const playerCardsContainer = document.querySelector('.js-player-cards-container');
+
+playerCardsContainer.innerHTML = 'ide jönnek majd a lapok';
+
+// program state 
+let deckId = null; 
+let playerCards = [];
+
+function renderPlayerCards() {
+    let html = '';
+
+    for (let card of playerCards) {
+        html += `<img src="${card.image}" alt="${card.code}" />`;
+    }
+    playerCardsContainer.innerHTML = html;
+}
+
+function drawAndRenderPlayerCards() {
+    if (deckId == null) return;
+    fetch(`https://deckofcardsapi.com/api/deck/${ deckId }/draw/?count=2`)
+        .then(data => data.json())
+        .then(function(response) {
+            playerCards = response.cards;
+            renderPlayerCards();
+        });   
+}
+
+function startGame() {
+    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
+        .then(data => data.json())
+        .then(function(response) {
+            deckId = response.deck_id;
+            drawAndRenderPlayerCards(); // TODO: refactorálás async-await segítségével 
+        });    
+}
+
+newGameButton.addEventListener('click', startGame);
