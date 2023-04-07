ec297c1748910b8cb74b19ab600f38bafc6df2d9
Zsolt Nagy <info@zsoltnagy.eu>
2023-04-07 18:39:11 +0200

    10. lépés: számítógép lapjainak megjelenítése

--- poker.js
+++ poker.js
@@ -1,44 +1,45 @@
-const newGameButton = document.querySelector('.js-new-game-button');
-const playerCardsContainer = document.querySelector('.js-player-cards-container');
-const chipCountContainer = document.querySelector('.js-chip-count-container');
-const potContainer = document.querySelector('.js-pot-container');
-const betArea = document.querySelector('.js-bet-area');
-const betSlider = document.querySelector('#bet-amount');
-const betSliderValue = document.querySelector('.js-slider-value');
-const betButton = document.querySelector('.js-bet-button');
+const newGameButton = document.querySelector(".js-new-game-button");
+const potContainer = document.querySelector(".js-pot-container");
+const betArea = document.querySelector(".js-bet-area");
+const betSlider = document.querySelector("#bet-amount");
+const betSliderValue = document.querySelector(".js-slider-value");
+const betButton = document.querySelector(".js-bet-button");
 
-// program state 
+const playerCardsContainer = document.querySelector(".js-player-cards-container");
+const playerChipContainer = document.querySelector(".js-player-chip-container");
+
+const computerCardsContainer = document.querySelector(".js-computer-cards-container");
+const computerChipContainer = document.querySelector(".js-computer-chip-container");
+const computerActionContainer = document.querySelector(".js-computer-action");
+
+// program state
 let {
     deckId,
-    playerCards,     // játékos lapjai
-    computerCards,   // számítógép lapjai (TODO: private? OOP??)
-    playerChips,     // játékos zsetonjai
-    computerChips,   // gép zsetonjai
+    playerCards, // játékos lapjai
+    computerCards, // számítógép lapjai (TODO: private? OOP??)
+    computerAction, // játékos cselekedete (call, fold)
+    playerChips, // játékos zsetonjai
+    computerChips, // gép zsetonjai
     playerBetPlaced, // játékos már licitált
-    pot              // kassza 
+    pot, // kassza
 } = getInitialState();
 
 function getInitialState() {
     return {
         deckId: null,
         playerCards: [],
+        computerCards: [],
+        computerAction: null,
         playerChips: 100,
         computerChips: 100,
         playerBetPlaced: false,
-        pot: 0
+        pot: 0,
     };
 }
 
 function initialize() {
-    ({ 
-        deckId, 
-        playerCards, 
-        playerChips, 
-        computerChips, 
-        playerBetPlaced,
-        pot 
-    } = getInitialState());
-
+    ({ deckId, playerCards, computerCards, computerAction, playerChips, computerChips, playerBetPlaced, pot } =
+        getInitialState());
 }
 
 function canBet() {
@@ -47,51 +48,63 @@ function canBet() {
 
 function renderSlider() {
     if (canBet()) {
-        betArea.classList.remove('invisible');
-        betSlider.setAttribute('max', playerChips); 
+        betArea.classList.remove("invisible");
+        betSlider.setAttribute("max", playerChips);
         betSliderValue.innerText = betSlider.value;
     } else {
-        betArea.classList.add('invisible');
+        betArea.classList.add("invisible");
     }
 }
 
-function renderPlayerCards() {
-    let html = '';
+function renderCardsInContainer(cards, container) {
+    let html = "";
 
-    for (let card of playerCards) {
+    for (let card of cards) {
         html += `<img src="${card.image}" alt="${card.code}" />`;
     }
-    playerCardsContainer.innerHTML = html;
+    container.innerHTML = html;
+}
+
+function renderAllCards() {
+    renderCardsInContainer(playerCards, playerCardsContainer);
+    renderCardsInContainer(computerCards, computerCardsContainer);
 }
 
 function renderChips() {
-    chipCountContainer.innerHTML = `
-        <div class="chip-count">Player chips: ${ playerChips }</div>
-        <div class="chip-count">Computer chips: ${ computerChips }</div>
+    playerChipContainer.innerHTML = `
+        <div class="chip-count">Játékos: ${playerChips} zseton</div>
+    `;
+    computerChipContainer.innerHTML = `
+        <div class="chip-count">Számítógép: ${computerChips} zseton</div>
     `;
 }
 
 function renderPot() {
     potContainer.innerHTML = `
-    <div class="chip-count">Pot: ${ pot }</div>
+    <div class="chip-count">Pot: ${pot}</div>
     `;
 }
 
+function renderActions() {
+    computerActionContainer.innerHTML = computerAction ?? "";
+}
+
 function render() {
-    renderPlayerCards();
+    renderAllCards();
     renderChips();
     renderPot();
     renderSlider();
+    renderActions();
 }
 
 function drawAndRenderPlayerCards() {
     if (deckId == null) return;
-    fetch(`https://deckofcardsapi.com/api/deck/${ deckId }/draw/?count=2`)
-        .then(data => data.json())
-        .then(function(response) {
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then((data) => data.json())
+        .then(function (response) {
             playerCards = response.cards;
             render();
-        });   
+        });
 }
 
 function postBlinds() {
@@ -102,51 +115,53 @@ function postBlinds() {
 }
 
 // Egy leosztás indítása
-function startHand() { // hand = leosztás
+function startHand() {
+    // hand = leosztás
     postBlinds(); // vaktétek adminisztrálása
-    fetch('https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1')
-        .then(data => data.json())
-        .then(function(response) {
+    fetch("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+        .then((data) => data.json())
+        .then(function (response) {
             deckId = response.deck_id;
-            drawAndRenderPlayerCards(); // TODO: refactorálás async-await segítségével 
-        });   
+            drawAndRenderPlayerCards(); // TODO: refactorálás async-await segítségével
+        });
 }
 
-// Egy játék egy vagy több leosztásból áll. 
+// Egy játék egy vagy több leosztásból áll.
 function startGame() {
     initialize();
     startHand();
 }
 
-function shouldComputerCall() {
+function shouldComputerCall(computerCards) {
     if (computerCards.length !== 2) return false; // extra védelem
-    const card1Code = computerCards[0].code;  // pl. AC, 4H, 9D, 0H (10: 0)
+    const card1Code = computerCards[0].code; // pl. AC, 4H, 9D, 0H (10: 0)
     const card2Code = computerCards[1].code;
     const card1Value = card1Code[0];
     const card2Value = card2Code[0];
     const card1Suit = card1Code[1];
     const card2Suit = card2Code[1];
 
-    return card1Value === card2Value ||
-           ['0', 'J', 'Q', 'K', 'A'].includes( card1Value ) || 
-           ['0', 'J', 'Q', 'K', 'A'].includes( card2Value ) ||
-           (
-               card1Suit === card2Suit &&
-               Math.abs(Number(card1Value) - Number(card2Value)) <= 2
-           );
+    return (
+        card1Value === card2Value ||
+        ["0", "J", "Q", "K", "A"].includes(card1Value) ||
+        ["0", "J", "Q", "K", "A"].includes(card2Value) ||
+        (card1Suit === card2Suit && Math.abs(Number(card1Value) - Number(card2Value)) <= 2)
+    );
 }
 
 function computerMoveAfterBet() {
-    fetch(`https://deckofcardsapi.com/api/deck/${ deckId }/draw/?count=2`)
-        .then(data => data.json())
-        .then(function(response) {
-            computerCards = response.cards;
-            alert(shouldComputerCall() ? 'Call' : 'Fold');
-            console.log(computerCards);
-            // render();
-        });    
+    fetch(`https://deckofcardsapi.com/api/deck/${deckId}/draw/?count=2`)
+        .then((data) => data.json())
+        .then(function (response) {
+            if (shouldComputerCall(response.cards)) {
+                computerAction = "Call";
+                computerCards = response.cards;
+            } else {
+                computerAction = "Fold";
+            }
+            render();
+        });
 }
-
 
 function bet() {
     const betValue = Number(betSlider.value);
@@ -154,7 +169,7 @@ function bet() {
     pot += betValue;
     // játékos zsetonjaiból kivonjuk a bet méretét
     playerChips -= betValue;
-    // játék állapota: játékos megtette a tétjét 
+    // játék állapota: játékos megtette a tétjét
     playerBetPlaced = true;
     // újrarenderelünk
     render();
@@ -162,8 +177,8 @@ function bet() {
     computerMoveAfterBet();
 }
 
-newGameButton.addEventListener('click', startGame);
-betSlider.addEventListener('change', render);
-betButton.addEventListener('click', bet);
+newGameButton.addEventListener("click", startGame);
+betSlider.addEventListener("change", render);
+betButton.addEventListener("click", bet);
 initialize();
 render();
